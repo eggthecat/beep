17825e75c8a542ad4bc209464fbbb49ef8306dc0
Dominic Montelongo <montelongodominic@gmail.com>
2019-08-23 11:42:33 -0700

    Add partial user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
-JESUS MARY AND JOSEPH!
+//Business Interface
+
+
+//User Interface
+$(document).ready(function(){
+  $("form#beepBoop").submit(function(event){
+    event.preventDefault();
+
+    var inputBBQ1 = parseInt($("#beepBoopQuestion1").val());
+    var outputBBR1 = 
+
+  });
+});
